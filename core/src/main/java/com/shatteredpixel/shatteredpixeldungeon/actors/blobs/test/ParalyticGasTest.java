594653abac29fbcc050722df8ad5762e004fa8fd
82109 <82109@192.168.200.167>
2023-05-15 02:13:00 +0900

    1. Refactoring 대상 : ConfusionGas, ParalyticGas, StenchGas 3개의 Class내의 함수 evolve, use, tileDesc 3개의 함수에 대하여 Refactoring 수행(모두 동일한 이름의 함수를 지님)
    ConfusionGas, ParalyticGas, StenchGas, three functions in the class,
    evolve, use, and tileDesc. Refactoring is performed for three functions
    (all have the same name function).
    
    2. 적용한 Refactoring Operation 이름
    : Move Method
    
    3. (1)의 Refactoring Operation을 적용한 이유
    :3개의 함수가 모두 중복되는 코드를 가지기 때문에 새로운 Class를 만들어서 옮김
    
    Since all three functions have redundant code, I created a new class and
    moved it.

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/blobs/test/ParalyticGasTest.java
@@ -0,0 +1,29 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors.blobs.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ParalyticGasTest {
+
+	@Test
+	void testGas_evolve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEvolve() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTileDesc() {
+		fail("Not yet implemented");
+	}
+
+}
