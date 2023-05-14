299c8594d4197709c7309e0a35615a6e9bd44861
jeong jin kyu <jjk3969@naver.com>
2023-05-15 02:26:58 +0900

    build.gradle initailize & crate buffTest.java

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/BuffTest.java
@@ -0,0 +1,27 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors.buffs;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuffTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void append() {
+    }
+
+    @Test
+    void testAppend() {
+
+    }
+}
